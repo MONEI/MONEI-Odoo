5b2545e3b17e2510296731a6e7f8b6672d4cb2f1
Felix Muñoz <fmunozdambo@gmail.com>
2025-06-30 11:25:07 +0200

    feat: add link to sale order option

--- monei/__manifest__.py
+++ monei/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'MONEI',
     'author': 'MONEI',
-    'version': '1.1.1',
+    'version': '1.0.0',
     'category': 'Accounting',
     'sequence': 350,
     'website': 'https://monei.com',
@@ -25,6 +25,7 @@ This module integrates MONEI payment services with Odoo.
         'views/refund_wizard_views.xml',
         'views/capture_wizard_views.xml',
         'views/sale_order_views.xml',
+        'views/link_order_wizard_views.xml',
     ],
     'application': True,
     'post_init_hook': 'post_init_hook',
